409b44e68cc1eee50d8d50f5f60db01573f69790
Tiatde <tiatde0323@gmail.com>
2023-12-13 20:37:59 +0800

    Initial Commit

---
+++ myFirst/myFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstApp.swift
+//  myFirst
+//
+//  Created by 訪客使用者 on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct myFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
